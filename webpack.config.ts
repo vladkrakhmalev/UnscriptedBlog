292bcf239b2e877d1e2c9499ff5fb5e662daea79
Влад Крахмалев <vlad.krakhmalev.web@gmail.com>
2025-01-30 22:49:50 +0300

    init: create webpack config

---
+++ webpack.config.ts
@@ -0,0 +1,26 @@
+import path from 'path'
+import { Configuration } from 'webpack'
+import { buildWebpackConfig } from './config/build/buildWebpackConfig'
+import { IBuildEnv, IBuildPaths } from './config/build/types/config'
+
+export default (env: IBuildEnv) => {
+
+  const paths: IBuildPaths = {
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    build: path.resolve(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+  }
+  
+  const mode = env.mode || 'development'
+  const PORT = env.port || 3000
+  const isDev = mode === 'development'
+  
+  const config: Configuration = buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT,
+  })
+
+  return config
+}
