e7dbb26d752bac7513a8921ad13c04c9b235ce28
Влад Крахмалев <vlad.krakhmalev.web@gmail.com>
2025-01-31 19:50:55 +0300

    build: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import classes from './index.modules.scss'
-import './global.scss'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { MainPageLazy } from './pages/main-page/MainPageLazy'
+import { AboutPageLazy } from './pages/about-page/AboutPageLazy'
+import { Suspense } from 'react'
 
 export const App = () => {
   
   return (
-    <>
-      <h1 className={classes.title}>Hello</h1>
-      <h2 className="title">Scss modules working</h2>
-    </>
+    <BrowserRouter>
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<MainPageLazy/>} />
+          <Route path='/about' element={<AboutPageLazy/>} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   )
 }
