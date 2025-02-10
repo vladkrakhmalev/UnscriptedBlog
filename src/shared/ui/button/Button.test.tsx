560e3f8c81010c15db9cbe904852086062f5b6a7
Влад Крахмалев <vlad.krakhmalev.web@gmail.com>
2025-02-10 14:01:43 +0300

    test: add Button test

---
+++ src/shared/ui/button/Button.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react"
+import { Button } from "./Button"
+import SunIcon from '../../../shared/assets/icons/sun.svg' 
+
+describe('Button', () => {
+  test('test render', () => {
+    render(<Button>TEST</Button>)
+    expect(screen.getByText('TEST')).toBeInTheDocument()
+  })
+
+  test('test with icon', () => {
+    render(<Button icon={<SunIcon/>}>TEST</Button>)
+    expect(screen.getByText('TEST')).toHaveClass('_icon')
+  })
+})
